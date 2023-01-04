404ee6f4e75bfeb7291970b5cca3cdcf6bdaf49f
samscreen9 <58282761+samscreen9@users.noreply.github.com>
2023-01-05 00:15:42 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,31 @@
 import { Component, VERSION } from '@angular/core';
+import {  OnInit } from '@angular/core';
 import{Observable,of,from,fromEvent} from 'rxjs';
+import{map} from 'rxjs/operators';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   name = 'Angular ' + VERSION.major;
   constructor(){}
 
-   source = of(1, 2, 3, 4, 5);
-   subscribe = this.source.subscribe(val => console.log(val));
+  ngOnInit() {
+   let source1 = of(1, 2, 3, 4, 5);
+   let subscribe1 = source1.subscribe(val => console.log(val));
 
-   source2 =from([1,2,3,4,5,6,7]);
-   subscribe2=this.source.subscribe(
+   let source2 =from([1,2,3,4,5,6,7]);
+   let subscribe2=source2.subscribe(
      (data)=>{console.log(data)}
    )
 
+    let buttonStream$ = fromEvent(document.querySelector('#btn1')! as HTMLButtonElement, 'click')
+        .subscribe(event => console.log(event));
+
+  }
+   
 }
+
 
 
