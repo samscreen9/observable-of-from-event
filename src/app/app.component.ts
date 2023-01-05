48bea5677a7299c00e707df7831f78dd516a64ed
samscreen9 <58282761+samscreen9@users.noreply.github.com>
2023-01-05 22:20:08 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, VERSION } from '@angular/core';
 import {  OnInit } from '@angular/core';
-import{Observable,of,from,fromEvent} from 'rxjs';
+import{Observable,of,from,fromEvent,interval, Subject, Subscription} from 'rxjs';
 import{map} from 'rxjs/operators';
 @Component({
   selector: 'my-app',
@@ -9,6 +9,7 @@ import{map} from 'rxjs/operators';
 })
 export class AppComponent implements OnInit  {
   name = 'Angular ' + VERSION.major;
+  subscription:Subscription;
   constructor(){}
 
   ngOnInit() {
@@ -38,9 +39,31 @@ export class AppComponent implements OnInit  {
       (data)=>{console.log(data);}
     )
 
+ /* observable with interval */
+ let itvr=interval(1200);
+ this.subscription= itvr.subscribe(
+    (data)=>{console.log(data);
+      this.addtoList(data,'unorderedList')
+      if(data>2){
+      this.subscription.unsubscribe();
+     }}
+   )  
+/* timer is same as interval except it takes first parameter as delay to start interval let x=timer(2000,1000)  timer(delay,interval) */
+
+    /* custom observable with i */
+
+    /* subject */
+   let s= new Subject<number>();
+
   }
 
-  
+  /*utilty function */
+  addtoList(item,appendto){
+  let li=document.createElement('li');
+  li.textContent=item;
+  let ap=document.getElementById(appendto);
+  ap.appendChild(li);
+  }
    
 }
 
